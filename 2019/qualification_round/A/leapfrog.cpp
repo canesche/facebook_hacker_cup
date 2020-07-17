008acaee7bd66ae98dd59422c1b733fdffce9893
canesche <michael.canesche@gmail.com>
2020-07-17 10:58:52 -0300

    test

---
+++ 2019/qualification_round/A/leapfrog.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    string s;
+    bool b;
+    
+    cin >> n;
+   	
+    
+    for(int i = 0; i < n; i++) {
+    	cin >> s;
+    	b = true;
+    	for(int j = 0; j < n; j++) {
+    		
+    	}
+    	cout << "Case #" << i+1 << ": ";
+    	if (b) cout << "Y" << endl;
+    	else cout << "N" << endl; 
+    }
+
+    return 0;
+}
